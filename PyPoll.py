b1de10b6c1ab9f5aa59a7444cb80d2c080e38c73
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-03-04 18:52:48 -0500

    Module 3

---
+++ PyPoll.py
@@ -0,0 +1,7 @@
+# The data we need to retrieve 
+# 1. The total number of votes casted
+# 2. A complete lists of canidates who recieved votes
+# 3. The percentage of votes of each canidate won
+# 4. The total number of votes of each canidate won
+# 5. The winner of the election based on popular vote 
+
--- Python_practice.py
+++ Python_practice.py
@@ -1,1 +1,86 @@
 print("Hello World")
+
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+
+temperature = int(input("What is the temperature outside? "))
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+
+
+    counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+
+#Example of While Loops
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+# This will not print, and changing to greater turns it into an infinite loop
+    count = 7
+
+while count < 1:
+
+    print("Hello World")
+
+for i in range(len(counties)):
+    print(counties[i])  
+
+
+# Get Each Dictionary in a List of Dictionaries
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+#Nested For loop
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+
+#Using F String with Dictionaries
+
+# No F String 
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+# Using F String 
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+# F strings in Multiple lines
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
